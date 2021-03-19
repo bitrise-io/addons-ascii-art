97e779c246c9c965d85b32c6f94ceb0979680640
Tamas Papik <trapacska@gmail.com>
2021-03-19 13:54:20 +0100

    added oauth2client

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,31 @@
 import express from 'express';
-import axios from 'axios';
+import axios, { AxiosInstance } from 'axios';
 
 const app = express();
 const port = process.env.PORT || 3000;
 const clientID = process.env.CLIENT_ID;
 const clientSecret = process.env.CLIENT_SECRET;
 const tokenBaseURL = process.env.TOKEN_BASE_URL || 'https://auth.services.bitrise.io'
+const apiBaseURL = process.env.API_BASE_URL || 'https://api.bitrise.io'
+
+let inmemStorage = {
+  // "test-app-slug": {
+  //   accessToken: "access-token",
+  //   refreshToken: "refresh-token",
+  // }
+};
+
+//
+// Routes
 
 app.get('/', (req, res) => {
   res.send('Welcome to ASCII art');
 });
 
+// -
+
 app.post('/provision', (req, res) => {
-  const token = getToken(req)
+  const token = getTokenFromHeader(req)
 
   if (!token) {
     res.status(401).send({ code: 400, message: 'invalid token in Authorization header' });
@@ -33,11 +46,14 @@ app.post('/provision', (req, res) => {
     }
   }
 
-  axios.post(tokenBaseURL+'/auth/realms/addons/protocol/openid-connect/token', params, config).then(function (response) {
+  axios.post(tokenBaseURL + '/auth/realms/addons/protocol/openid-connect/token', params, config).then(function (response) {
     const accessToken = response.data.access_token;
     const refreshToken = response.data.refresh_token;
 
-    console.log({'accessToken': accessToken, 'refreshToken': refreshToken});
+    inmemStorage[req.body.app_slug] = {
+      accessToken: accessToken,
+      refreshToken: refreshToken,
+    }
 
     res.sendStatus(200)
   }).catch(function (error) {
@@ -45,18 +61,37 @@ app.post('/provision', (req, res) => {
   });
 });
 
+// -
+
 app.post('/login', (req, res) => {
   res.send('hi meercode');
 });
 
+// -
+
 app.delete('/provision/{app_slug}', (req, res) => {
 });
+
+// -
+
+app.get('/asciiart/{app_slug}', (req, res) => {
+  const axiosApiInstance = getOAuth2ClientForAppSlug(req.params.app_slug)
+
+  axiosApiInstance.get(apiBaseURL+ '/apps/' + req.params.app_slug).then(function (response) {
+    res.send(response.data['data'].title)
+  }).catch(function (error) {
+    res.status(error.response.status).send(error.response.data);
+  });
+});
+
+//
+// main
 
 app.listen(port, () => {
   return console.log(`server is listening on ${port}`);
 });
 
-let getToken = (req: any): string | undefined => {
+let getTokenFromHeader = (req: any): string | undefined => {
   const bearerPrefixLen = 'bearer '.length
   let token = req.headers.authorization
 
@@ -65,4 +100,62 @@ let getToken = (req: any): string | undefined => {
   }
 
   return token.substr(bearerPrefixLen);
+}
+
+let refreshAccessToken = (appSlug: string): string => {
+  const params = new URLSearchParams({
+    'grant_type': 'refresh_token',
+    'client_id': clientID,
+    'refresh_token': inmemStorage[appSlug].refreshToken,
+  });
+
+  const config = {
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    }
+  }
+
+  axios.post(tokenBaseURL + '/auth/realms/addons/protocol/openid-connect/token', params, config).then(function (response) {
+    const accessToken = response.data.access_token;
+    const refreshToken = response.data.refresh_token;
+
+    inmemStorage[appSlug] = {
+      accessToken: accessToken,
+      refreshToken: refreshToken,
+    }
+
+    return accessToken
+  }).catch(function (error) {
+    console.log(error.response.status, error.response.data);
+  });
+  return ''
+}
+
+let getOAuth2ClientForAppSlug = (appSlug: string): AxiosInstance => {
+  const axiosApiInstance = axios.create();
+  axiosApiInstance.interceptors.request.use(
+    async config => {
+      config.headers = {
+        'Authorization': `Bearer ` + inmemStorage[appSlug].accessToken,
+      }
+      return config;
+    },
+    error => {
+      Promise.reject(error)
+    });
+
+  axiosApiInstance.interceptors.response.use((response) => {
+    return response
+  }, async function (error) {
+    const originalRequest = error.config;
+    if (error.response.status === 403 && !originalRequest._retry) {
+      originalRequest._retry = true;
+      const access_token = refreshAccessToken(appSlug);
+      originalRequest.headers['Authorization'] = 'Bearer ' + access_token;
+      return axiosApiInstance(originalRequest);
+    }
+    return Promise.reject(error);
+  });
+
+  return axiosApiInstance;
 }
