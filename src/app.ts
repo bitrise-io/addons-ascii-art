9f7914b15dfa2fbaa34b4293ef3f5f13cdb96c80
adam.pelle <adam.pelle@bitrise.io>
2021-03-19 10:56:06 +0100

    initial commit

---
+++ src/app.ts
@@ -0,0 +1,23 @@
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Welcome to ASCII art');
+});
+
+app.post('/provision', (req, res) => {
+  res.sendStatus(200);
+});
+
+app.post('/login', (req, res) => {
+  res.send('hi meercode');
+});
+
+app.delete('/provision/{app_slug}', (req, res) => {
+});
+
+app.listen(port, () => {
+  return console.log(`server is listening on ${port}`);
+});
