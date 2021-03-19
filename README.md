e4cca90b436820631887930b4dedea4a7243a16d
Tamas Papik <trapacska@gmail.com>
2021-03-19 12:59:22 +0100

    added description and provisioning logic

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-Hi
+# Usage
+
+## Development
+
+1. Run `yarn install`
+1. Export envs: `CLIENT_ID` and `CLIENT_SECRET`
+1. Run `yarn dev`
+
+or 
+
+1. Configure your .bitrise.secrets.yml:
+    ```
+    envs: 
+    - CLIENT_ID: ...
+    - CLIENT_SECRET: ...
+    ```
+1. Run `bitrise run up`
+
+The service should be ready now. You can try running command: `curl localhost:3000` to see if the service responds.
+
+# Provisioning
+
+```
+curl -X POST 'http://localhost:3000/provision' --header 'Authorization: Bearer <subject_token>'
+```
--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,47 @@
 import express from 'express';
+import axios from 'axios';
 
 const app = express();
 const port = process.env.PORT || 3000;
- 
+const clientID = process.env.CLIENT_ID;
+const clientSecret = process.env.CLIENT_SECRET;
+
 app.get('/', (req, res) => {
   res.send('Welcome to ASCII art');
 });
 
 app.post('/provision', (req, res) => {
-  res.sendStatus(200);
+  const token = getToken(req)
+
+  if (!token) {
+    res.status(401).send({ code: 400, message: 'invalid token in Authorization header' });
+    return;
+  }
+
+  const params = new URLSearchParams({
+    'grant_type': 'urn:ietf:params:oauth:grant-type:token-exchange',
+    'client_id': clientID,
+    'client_secret': clientSecret,
+    'subject_token': token,
+    'requested_token_type': 'urn:ietf:params:oauth:token-type:refresh_token'
+  });
+
+  const config = {
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    }
+  }
+
+  axios.post('https://auth.services.bitrise.dev/auth/realms/addons/protocol/openid-connect/token', params, config).then(function (response) {
+    const accessToken = response.data.access_token;
+    const refreshToken = response.data.refresh_token;
+
+    console.log({'accessToken': accessToken, 'refreshToken': refreshToken});
+
+    res.sendStatus(200)
+  }).catch(function (error) {
+    res.status(error.response.status).send(error.response.data);
+  });
 });
 
 app.post('/login', (req, res) => {
@@ -21,3 +54,14 @@ app.delete('/provision/{app_slug}', (req, res) => {
 app.listen(port, () => {
   return console.log(`server is listening on ${port}`);
 });
+
+let getToken = (req: any): string | undefined => {
+  const bearerPrefixLen = 'bearer '.length
+  let token = req.headers.authorization
+
+  if (!token || token.length < bearerPrefixLen) {
+    return undefined;
+  }
+
+  return token.substr(bearerPrefixLen);
+}
